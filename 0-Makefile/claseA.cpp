736c8c9f9f8ce647d053c8bd92691ef4983ec19b
A01752113 <A01752113@itesm.mx>
2021-05-10 22:13:35 -0500

    ejercicio

---
+++ 0-Makefile/claseA.cpp
@@ -0,0 +1,12 @@
+#include "claseA.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+ClaseA::ClaseA(){
+    cout<<"Constructor clase A"<<endl;
+}
+void ClaseA::imprimir(){
+    cout<<"Metodo imprimir de la clase A"<<endl;
+}
---
+++ 0-Makefile/claseB.cpp
@@ -0,0 +1,12 @@
+#include "claseB.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+ClaseB::ClaseB(){
+    cout<<"Constructor clase B"<<endl;
+}
+void ClaseB::imprimir(){
+    cout<<"Metodo imprimir de la clase B"<<endl;
+}
